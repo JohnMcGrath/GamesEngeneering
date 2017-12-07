64a1c2b4acaecff8d23a27fe19a9110024ba3f59
JohnMcGrath <mcgrathjohnjerome@gmail.com>
2017-12-07 10:40:48 +0000

    Minor Updates

--- LabUnitTest/RWM_1718_P2_B/unitTestRenderSystem/unitTestRenderSystem.cpp
+++ LabUnitTest/RWM_1718_P2_B/unitTestRenderSystem/unitTestRenderSystem.cpp
@@ -1,6 +1,7 @@
 #include "gtest\gtest.h"
 #include "RenderSystem.h"
 #include "RenderComponent.h"
+#include "AIComponent.h"
 
 TEST(testRender, simpleTest) 
 {
@@ -8,7 +9,7 @@ TEST(testRender, simpleTest)
 	EXPECT_EQ(false, rnd.getfoundComponent());
 }
 
-TEST(testRender, entityTest)
+TEST(testRender, entityCorrectTest)
 {
 	RenderSystem* rnd = new RenderSystem();
 	Entity* player = new Entity();
@@ -18,4 +19,50 @@ TEST(testRender, entityTest)
 	rnd->update();
 	
 	EXPECT_EQ(true, rnd->getfoundComponent());
+}
+
+TEST(testRender, entityWrongComponentTest)
+{
+	RenderSystem* rnd = new RenderSystem();
+	Entity* player = new Entity();
+	AIComponent* aiCom = new AIComponent();
+	player->addComponent(aiCom);
+	rnd->addEntity(player);
+	rnd->update();
+
+	EXPECT_EQ(false, rnd->getfoundComponent());
+}
+
+TEST(testRender, entityNotPresentTest)
+{
+	RenderSystem* rnd = new RenderSystem();
+	Entity* player = new Entity();
+	rnd->update();
+
+	EXPECT_EQ(false, rnd->getfoundComponent());
+}
+
+TEST(testRender, entityMultipleComponentsTest)
+{
+	RenderSystem* rnd = new RenderSystem();
+	Entity* player = new Entity();
+	AIComponent* aiCom = new AIComponent();
+	RenderComponent* rnCom = new RenderComponent();
+
+	player->addComponent(aiCom);
+	player->addComponent(rnCom);
+	rnd->addEntity(player);
+	rnd->update();
+
+	EXPECT_EQ(true, rnd->getfoundComponent());
+}
+
+TEST(testRender, entityNoComponentTest)
+{
+	RenderSystem* rnd = new RenderSystem();
+	Entity* player = new Entity();
+	rnd->addEntity(player);
+	rnd->update();
+
+	EXPECT_EQ(false, rnd->getfoundComponent());
 }
